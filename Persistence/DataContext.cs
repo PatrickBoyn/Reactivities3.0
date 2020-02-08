43dc9286975822e0ab05c61f30a9158e4ce17324
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-08 00:22:58 -0500

    Added the db set and the data context ctor.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
     }
 }
