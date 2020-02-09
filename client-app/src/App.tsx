d61606665b3f54c6dc2a7fe76b4e2cb954cd9734
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-09 17:22:02 -0500

    Added the logic that displays values from a list.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    values: []
+  };
+
+  componentDidMount() {
+    this.setState({
+      values: [
+        { id: 1, name: 'Value 101' },
+        { id: 2, name: 'Value 102' },
+        { id: 3, name: 'Value 103' }
+      ]
+    });
+  }
   render() {
-    return <div className='App'></div>;
+    return (
+      <div className='App'>
+        <ul>
+          {this.state.values.map((value: any) => (
+            <li>{value.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
