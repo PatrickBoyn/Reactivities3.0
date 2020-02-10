95d5285a00c297a342f8345fb2e7b6d7702b3fd6
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-09 23:02:49 -0500

    Used semantic ui components.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react';
 
 class App extends Component {
   state = {
@@ -16,12 +17,16 @@ class App extends Component {
   }
   render() {
     return (
-      <div className='App'>
-        <ul>
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
           {this.state.values.map((value: any) => (
-            <li key={value.id}>{value.name}</li>
+            <List.Item key={value.id}>{value.name}</List.Item>
           ))}
-        </ul>
+        </List>
       </div>
     );
   }
