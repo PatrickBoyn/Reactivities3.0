91160da76da70f4f1511186214f0726c87bca2fa
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-12 15:08:28 -0500

    Replaced the div with a fragment, and added a container.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
-import { List } from 'semantic-ui-react';
+import { List, Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import Navbar from '../../features/nav-bar/NavBar';
 
@@ -14,14 +14,17 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <Fragment>
       <Navbar />
-      <List>
-        {activities.map(activity => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+      {/* I don't normally use inline styles. */}
+      <Container style={{ marginTop: '7em' }}>
+        <List>
+          {activities.map(activity => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container>
+    </Fragment>
   );
 };
 
