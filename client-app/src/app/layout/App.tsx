5fa4282db20e3e4e62395b142600ae5aa4a76266
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-12 00:50:34 -0500

    Updated the requests to get activities.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,13 +4,13 @@ import { Header, Icon, List } from 'semantic-ui-react';
 
 class App extends Component {
   state = {
-    values: []
+    activities: []
   };
 
   componentDidMount() {
-    axios.get('/api/values').then(response => {
+    axios.get('/api/activities').then(response => {
       this.setState({
-        values: response.data
+        activities: response.data
       });
     });
   }
@@ -22,8 +22,8 @@ class App extends Component {
           <Header.Content>Reactivities</Header.Content>
         </Header>
         <List>
-          {this.state.values.map((value: any) => (
-            <List.Item key={value.id}>{value.name}</List.Item>
+          {this.state.activities.map((activity: any) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
         </List>
       </div>
