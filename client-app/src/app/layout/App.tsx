60cae3db6d046901e18b05063a679924c54d492a
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-12 23:45:56 -0500

    Passed the ability to select an activity down to the dashboard.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
-import { List, Container } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import Navbar from '../../features/nav-bar/NavBar';
 import ActivityDashboard from '../../features/Activities/Dashboard/ActivityDashboard';
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter(a => a.id === id)[0]);
+  };
 
   useEffect(() => {
     axios.get<IActivity[]>('/api/activities').then(response => {
@@ -19,7 +26,11 @@ const App = () => {
       <Navbar />
       {/* I don't normally use inline styles. */}
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard activities={activities} />
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity}
+        />
       </Container>
     </Fragment>
   );
