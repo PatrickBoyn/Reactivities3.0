69739057d8f22cc0bb4c186ed0e0dd93861ba9ed
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-13 16:24:50 -0500

    Added the setSelectActivity to the button.

--- client-app/src/features/Activities/Details/ActivityDetails.tsx
+++ client-app/src/features/Activities/Details/ActivityDetails.tsx
@@ -5,11 +5,13 @@ import { IActivity } from '../../../app/models/activity';
 interface IProps {
   selectedActivity: IActivity;
   setEditMode: (editMode: boolean) => void;
+  setSelectedActivity: (activity: IActivity | null) => void;
 }
 
 const ActivityDetails: React.FC<IProps> = ({
   selectedActivity,
-  setEditMode
+  setEditMode,
+  setSelectedActivity
 }) => {
   return (
     <Card fluid>
@@ -33,7 +35,12 @@ const ActivityDetails: React.FC<IProps> = ({
             color='blue'
             content='Edit'
           />
-          <Button basic color='grey' content='Cancel' />
+          <Button
+            onClick={() => setSelectedActivity(null)}
+            basic
+            color='grey'
+            content='Cancel'
+          />
         </Button.Group>
       </Card.Content>
     </Card>
