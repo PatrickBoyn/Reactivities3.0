455712ad68cfecc9a026eb9f948da7c2b2987932
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-12 23:46:17 -0500

    Created the ability for the user to select an activity.

--- client-app/src/features/Activities/Dashboard/ActivityDashboard.tsx
+++ client-app/src/features/Activities/Dashboard/ActivityDashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, GridColumn, List } from 'semantic-ui-react';
+import { Grid, GridColumn } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
 import ActivityDetails from '../Details/ActivityDetails';
@@ -7,16 +7,24 @@ import ActivityForm from '../Form/ActivityForm';
 
 interface IProps {
   activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null;
 }
 
-const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
+const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity
+}) => {
   return (
     <Grid>
       <GridColumn width={10}>
-        <ActivityList activities={activities} />
+        <ActivityList activities={activities} selectActivity={selectActivity} />
       </GridColumn>
       <GridColumn width={6}>
-        <ActivityDetails />
+        {selectedActivity && (
+          <ActivityDetails selectedActivity={selectedActivity} />
+        )}
         <ActivityForm />
       </GridColumn>
     </Grid>
