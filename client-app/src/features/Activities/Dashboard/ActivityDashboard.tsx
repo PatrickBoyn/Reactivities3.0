b3c08bb0563f744d945f4f6c44dcba6e362af911
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-18 00:20:20 -0500

    Added back the  props.

--- client-app/src/features/Activities/Dashboard/ActivityDashboard.tsx
+++ client-app/src/features/Activities/Dashboard/ActivityDashboard.tsx
@@ -9,6 +9,8 @@ import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../../app/stores/activityStore';
 
 interface IProps {
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
   setEditMode: (editMode: boolean) => void;
   setSelectedActivity: (activity: IActivity | null) => void;
   createActivity: (activity: IActivity) => void;
