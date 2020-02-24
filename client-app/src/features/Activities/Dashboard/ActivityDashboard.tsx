ed8e1b5fc59b1638747e6531b14b81f132f026f4
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-24 17:48:27 -0500

    Removed unused imports.

--- client-app/src/features/Activities/Dashboard/ActivityDashboard.tsx
+++ client-app/src/features/Activities/Dashboard/ActivityDashboard.tsx
@@ -1,15 +1,9 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Grid, GridColumn } from 'semantic-ui-react';
 import ActivityList from './ActivityList';
-import ActivityDetails from '../Details/ActivityDetails';
-import ActivityForm from '../Form/ActivityForm';
 import { observer } from 'mobx-react-lite';
-import ActivityStore from '../../../app/stores/activityStore';
 
 const ActivityDashboard: React.FC = () => {
-  const activityStore = useContext(ActivityStore);
-  const { editMode, activity } = activityStore;
-
   return (
     <Grid>
       <GridColumn width={10}>
