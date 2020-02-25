44d067aefbe767bed8156aa75ba5412b7ed985d9
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-25 02:22:54 -0500

    Moved the activity loader, and spinner.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useContext, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Container } from 'semantic-ui-react';
 import Navbar from '../../features/nav-bar/NavBar';
 import ActivityDashboard from '../../features/Activities/Dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import ActivityStore from '../stores/activityStore';
 import { observer } from 'mobx-react-lite';
 import { Route, withRouter, RouteComponentProps } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
@@ -11,15 +9,6 @@ import ActivityForm from '../../features/Activities/Form/ActivityForm';
 import ActivityDetails from '../../features/Activities/Details/ActivityDetails';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
-  const activityStore = useContext(ActivityStore);
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial)
-    return <LoadingComponent content='Loading activities...' />;
-
   return (
     <Fragment>
       <Route exact path='/' component={HomePage} />
