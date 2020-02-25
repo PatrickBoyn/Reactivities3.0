43dd59a12caf7982135c4959bfa5a5452ccc5706
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-24 20:22:45 -0500

    Fixed an issue with the url.

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -38,7 +38,8 @@ const Activities = {
   list: (): Promise<IActivity[]> => requests.get('/activities'),
   details: (id: string) => requests.get(`/activities/${id}`),
   create: (activity: IActivity) => requests.post('/activities', activity),
-  update: (activity: IActivity) => requests.put('/activities', activity),
+  update: (activity: IActivity) =>
+    requests.put(`/activities/${activity.id}`, activity),
   delete: (id: string) => requests.delete(`/activities/${id}`)
 };
 
