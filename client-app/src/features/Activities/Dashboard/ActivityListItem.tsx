43006e45e6cc92f2c49cfab247d1d3ad83d25acb
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-25 17:13:36 -0500

    Restyled the item.

--- client-app/src/features/Activities/Dashboard/ActivityListItem.tsx
+++ client-app/src/features/Activities/Dashboard/ActivityListItem.tsx
@@ -1,34 +1,41 @@
 import React from 'react';
-import { Item, ItemContent, Button, Label } from 'semantic-ui-react';
+import { Item, ItemContent, Button, Segment, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { IActivity } from '../../../app/models/activity';
 
 const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
   return (
-    <Item key={activity.id}>
-      <ItemContent>
-        <ItemContent>
-          <Item.Header as='a'>{activity.title}</Item.Header>
-          <Item.Meta>{activity.date}</Item.Meta>
-          <Item.Description>
-            <div>{activity.description}</div>
-            <div>
-              {activity.city}, {activity.venue}
-            </div>
-          </Item.Description>
-          <Item.Extra>
-            <Button
-              as={Link}
-              to={`/activities/${activity.id}`}
-              floated='right'
-              content='View'
-              color='blue'
-            />
-            <Label basic content={activity.category} />
-          </Item.Extra>
-        </ItemContent>
-      </ItemContent>
-    </Item>
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item>
+            <Item.Image size='tiny' circular src='/assets/user.png' />
+            <ItemContent>
+              <ItemContent>
+                <Item.Header as='a'>{activity.title}</Item.Header>
+
+                <Item.Description>Hosted By Bob</Item.Description>
+              </ItemContent>
+            </ItemContent>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <Segment>
+        <Icon name='clock' /> {activity.date}
+        <Icon name='marker' /> {activity.venue}, {activity.city}
+      </Segment>
+      <Segment secondary>Attendees will go here</Segment>
+      <Segment clearing>
+        <span>{activity.description}</span>
+        <Button
+          as={Link}
+          to={`/activities/${activity.id}`}
+          floated='right'
+          content='View'
+          color='blue'
+        />
+      </Segment>
+    </Segment.Group>
   );
 };
 
