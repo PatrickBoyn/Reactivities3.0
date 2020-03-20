3ec4630c386e65551587c2ea9e334af58d6c5f48
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-03-20 18:05:34 -0400

    Created a JWT.

--- Infrastructure/Security/JwtGenerator.cs
+++ Infrastructure/Security/JwtGenerator.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Application.Interfaces;
 using Domain;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.Security
 {
@@ -7,7 +13,24 @@ namespace Infrastructure.Security
     {
         public string CreateToken(AppUser user)
         {
-            throw new System.NotImplementedException();
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
