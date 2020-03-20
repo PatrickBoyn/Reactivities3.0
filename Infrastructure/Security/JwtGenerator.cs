c4f7e374a1f7ac30f9bb470b37ad6abee3d0064a
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-03-20 15:17:58 -0400

    Implimented the interface.

--- Infrastructure/Security/JwtGenerator.cs
+++ Infrastructure/Security/JwtGenerator.cs
@@ -1,7 +1,13 @@
+using Application.Interfaces;
+using Domain;
+
 namespace Infrastructure.Security
 {
-    public class JwtGenerator
+    public class JwtGenerator : IJwtGenerator
     {
-        
+        public string CreateToken(AppUser user)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
