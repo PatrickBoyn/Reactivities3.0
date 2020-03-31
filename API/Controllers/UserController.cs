e69ad43d8ef7c4628b291cb7e4642686deb61dab
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-03-31 14:30:02 -0400

    Moved the AllowAnonymus, and created a get request.

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -5,19 +5,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
+
     public class UserController : BaseController
     {
+        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Query query)
         {
             return await Mediator.Send(query);
         }
 
+        [AllowAnonymous]
         [HttpPost("register")]
         public async Task<ActionResult<User>> RegisterUser(Register.Command command)
         {
             return await Mediator.Send(command);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User>> GetUser()
+        {
+            return await Mediator.Send(new CurrentUser.Query());
         }
     }
 }
