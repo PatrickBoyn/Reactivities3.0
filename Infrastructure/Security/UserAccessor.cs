943d059b66619d4d59945809d8f2802fc45549b5
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-03-31 02:02:24 -0400

    Created a class that will use the IUserAccessor interface.

---
+++ Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,12 @@
+using Application.Interfaces;
+
+namespace Infrastructure.Security
+{
+    public class UserAccessor : IUserAccessor
+    {
+        public string GetCurrentUser()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
