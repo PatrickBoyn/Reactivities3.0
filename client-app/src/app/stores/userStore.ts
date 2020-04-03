e0912158f19061d9171282ce753243f2f6e2fe7a
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-04-03 18:44:32 -0400

    Added the logic for the user store.

---
+++ client-app/src/app/stores/userStore.ts
@@ -0,0 +1,17 @@
+﻿import { action, computed, observable } from "mobx";
+import { IUser, IUserFormValues } from "../models/user";
+import agent from "../api/agent";
+
+export default class UserStore {
+	@observable user: IUser | null = null;
+	
+	@computed get isLoggedIn() { return !!this.user}
+	
+	@action login = async (values: IUserFormValues) => {
+		try {
+			this.user = await agent.User.login(values);
+		} catch (error) {
+			console.log(error)
+		}
+	}
+}
